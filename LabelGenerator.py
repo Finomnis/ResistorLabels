49413e022efc3501dcf298c2715ff496c527ece2
Finomnis <finomnis@gmail.com>
2020-09-20 19:09:20 +0200

    Working, needs minor adjustents

--- LabelGenerator.py
+++ LabelGenerator.py
@@ -3,7 +3,7 @@ from reportlab.lib.pagesizes import letter
 from reportlab.lib.units import inch
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
-from reportlab.lib.colors import black, toColor, HexColor
+from reportlab.lib.colors import black, toColor, HexColor, gray
 
 import math
 
@@ -106,19 +106,69 @@ def resistor_color_table_2(num):
     ][num]
 
 
-def resistor_color_table_3(num):
-    return [
-        HexColor("#995817"),
-        HexColor("#FF9900"),  # HexColor("#996633"),
-        HexColor("#994d00"),
-        HexColor("#995c1f"),
-        HexColor("#996026"),
-        HexColor("#000000"),
-        HexColor("#995008"),
-        HexColor("#996633"),
-        HexColor("#CCCCCC"),
-        HexColor("#FFFFFF"),
-    ][num]
+def draw_resistor_stripe(c, x, y, width, height, stripe_value):
+
+    if 0 <= stripe_value <= 9:
+        c.setFillColor(resistor_color_table_2(stripe_value))
+        c.rect(x, y, width, height, fill=1, stroke=0)
+        return
+
+    elif stripe_value == -1:
+        gold_table = [
+            HexColor("#fefefe"),
+            HexColor("#f7febe"),
+            HexColor("#effe41"),
+            HexColor("#c5d24f"),
+        ]
+
+        # Gold
+        c.setFillColor(gold_table[2])
+        c.rect(x, y+height*5/6, width, height/6, fill=1, stroke=0)
+        c.setFillColor(gold_table[1])
+        c.rect(x, y+height*4/6, width, height/6, fill=1, stroke=0)
+        c.setFillColor(gold_table[0])
+        c.rect(x, y+height*3/6, width, height/6, fill=1, stroke=0)
+        c.setFillColor(gold_table[1])
+        c.rect(x, y+height*2/6, width, height/6, fill=1, stroke=0)
+        c.setFillColor(gold_table[2])
+        c.rect(x, y+height*1/6, width, height/6, fill=1, stroke=0)
+        c.setFillColor(gold_table[3])
+        c.rect(x, y+height*0/6, width, height/6, fill=1, stroke=0)
+
+        return
+
+    elif stripe_value == -2:
+        silver_table = [
+            HexColor("#fefefe"),
+            HexColor("#e0e0e0"),
+            HexColor("#cdcdcd"),
+            HexColor("#b5b5b5"),
+        ]
+
+        # Silver
+        c.setFillColor(silver_table[2])
+        c.rect(x, y+height*5/6, width, height/6, fill=1, stroke=0)
+        c.setFillColor(silver_table[1])
+        c.rect(x, y+height*4/6, width, height/6, fill=1, stroke=0)
+        c.setFillColor(silver_table[0])
+        c.rect(x, y+height*3/6, width, height/6, fill=1, stroke=0)
+        c.setFillColor(silver_table[1])
+        c.rect(x, y+height*2/6, width, height/6, fill=1, stroke=0)
+        c.setFillColor(silver_table[2])
+        c.rect(x, y+height*1/6, width, height/6, fill=1, stroke=0)
+        c.setFillColor(silver_table[3])
+        c.rect(x, y+height*0/6, width, height/6, fill=1, stroke=0)
+
+        return
+
+    else:
+        c.setFillColor(gray, 0.3)
+        c.setStrokeColor(black, 0.5)
+        c.rect(x, y, width, height, fill=1, stroke=1)
+        c.line(x, y, x + width, y + height)
+        c.line(x + width, y, x, y + height)
+        pass
+
 
 def draw_resistor_colorcode(c, value, color1, color2, x, y, width, height, num_codes):
 
@@ -145,18 +195,119 @@ def draw_resistor_colorcode(c, value, color1, color2, x, y, width, height, num_c
                 stripe_value //= 10
             stripe_value %= 10
 
-        if stripe_value >= 0:
-            c.setFillColor(resistor_color_table_2(stripe_value))
-            c.rect(x+border+corner + stripe_width/2 + 2*stripe_width*i, y+border, stripe_width, height-2*border,
-                   fill=1, stroke=0)
-        else:
-            pass
+        draw_resistor_stripe(c,
+                             x + border + corner + stripe_width / 2 + 2 * stripe_width * i,
+                             y + border,
+                             stripe_width,
+                             height - 2 * border,
+                             stripe_value)
+
+    draw_resistor_stripe(c,
+                         x + width - border - corner - stripe_width * 1.5,
+                         y + border,
+                         stripe_width,
+                         height - 2 * border,
+                         -3)
 
     c.setFillColor(black)
-
     c.setStrokeColor(black, 1)
     c.setLineWidth(0.5)
     c.roundRect(x+border, y+border, width-2*border, height-2*border, corner)
+
+
+def get_3digit_code(value):
+    if value.ohms_val % 10 != 0:
+        return ""
+
+    digits = str(value.ohms_val // 10)
+
+    if value.ohms_exp > 0:
+        multiplier = str(value.ohms_exp - 1)
+        return digits + multiplier
+
+    if value.ohms_exp == 0:
+        return digits[0] + "R" + digits[1]
+
+    if value.ohms_exp == -1:
+        return "R" + digits
+
+    if value.ohms_exp == -2:
+        if value.ohms_val % 100 != 0:
+            return ""
+        return "R0" + digits[0]
+
+    return ""
+
+
+def get_4digit_code(value):
+    digits = str(value.ohms_val)
+
+    if value.ohms_exp > 1:
+        multiplier = str(value.ohms_exp - 2)
+        return digits + multiplier
+
+    if value.ohms_exp == 1:
+        return digits[0] + digits[1] + "R" + digits[2]
+
+    if value.ohms_exp == 0:
+        return digits[0] + "R" + digits[1] + digits[2]
+
+    if value.ohms_exp == -1:
+        return "R" + digits
+
+    if value.ohms_exp == -2:
+        if value.ohms_val % 10 != 0:
+            return ""
+        return "R0" + digits[0] + digits[1]
+
+    if value.ohms_exp == -3:
+        if value.ohms_val % 100 != 0:
+            return ""
+        return "R00" + digits[0]
+
+    return ""
+
+
+def get_eia98_code(value):
+    eia98_coding_table = {
+        100: "01", 178: "25", 316: "49", 562: "73",
+        102: "02", 182: "26", 324: "50", 576: "74",
+        105: "03", 187: "27", 332: "51", 590: "75",
+        107: "04", 191: "28", 340: "52", 604: "76",
+        110: "05", 196: "29", 348: "53", 619: "77",
+        113: "06", 200: "30", 357: "54", 634: "78",
+        115: "07", 205: "31", 365: "55", 649: "79",
+        118: "08", 210: "32", 374: "56", 665: "80",
+        121: "09", 215: "33", 383: "57", 681: "81",
+        124: "10", 221: "34", 392: "58", 698: "82",
+        127: "11", 226: "35", 402: "59", 715: "83",
+        130: "12", 232: "36", 412: "60", 732: "84",
+        133: "13", 237: "37", 422: "61", 750: "85",
+        137: "14", 243: "38", 432: "62", 768: "86",
+        140: "15", 249: "39", 442: "63", 787: "87",
+        143: "16", 255: "40", 453: "64", 806: "88",
+        147: "17", 261: "41", 464: "65", 825: "89",
+        150: "18", 267: "42", 475: "66", 845: "90",
+        154: "19", 274: "43", 487: "67", 866: "91",
+        158: "20", 280: "44", 499: "68", 887: "92",
+        162: "21", 287: "45", 511: "69", 909: "93",
+        165: "22", 294: "46", 523: "70", 931: "94",
+        169: "23", 301: "47", 536: "71", 953: "95",
+        174: "24", 309: "48", 549: "72", 976: "96",
+    }
+
+    if value.ohms_val not in eia98_coding_table:
+        return ""
+
+    digits = eia98_coding_table[value.ohms_val]
+
+    multiplier_table = ["Z", "Y", "X", "A", "B", "C", "D", "E", "F"]
+    if not (0 <= value.ohms_exp+1 < len(multiplier_table)):
+        return ""
+
+    multiplier = multiplier_table[value.ohms_exp+1]
+
+    return digits + multiplier
 
 
 def draw_resistor_sticker(c, row, column, ohms):
@@ -175,6 +326,7 @@ def draw_resistor_sticker(c, row, column, ohms):
 
     value_font_size = 0.25 * inch
     ohm_font_size = 0.15 * inch
+    smd_font_size = 0.08 * inch
     space_between = 5
 
     value_string = resistor_value.format_value()
@@ -199,7 +351,7 @@ def draw_resistor_sticker(c, row, column, ohms):
                             # toColor("hsl(55, 54%, 82%)"), toColor("hsl(55, 54%, 50%)"),
                             toColor("hsl(55, 54%, 100%)"), toColor("hsl(55, 54%, 70%)"),
                             rect.left + rect.width/2,
-                            rect.bottom + rect.height/4,
+                            rect.bottom + rect.height/4 - rect.height/45,
                             rect.width/4, rect.height/4,
                             3)
 
@@ -211,35 +363,49 @@ def draw_resistor_sticker(c, row, column, ohms):
                             # toColor("hsl(197, 59%, 85%)"), toColor("hsl(197, 59%, 60%)"),
                             toColor("hsl(197, 59%, 100%)"), toColor("hsl(197, 59%, 73%)"),
                             rect.left + rect.width * 0.75,
-                            rect.bottom + rect.height/4,
+                            rect.bottom + rect.height/4 - rect.height/45,
                             rect.width/4, rect.height/4,
                             4)
 
+    c.setFont('Arial Bold', smd_font_size * 1.35)
+    c.drawString(rect.left + rect.width/2 + rect.width/32, rect.bottom + rect.height/13, get_3digit_code(resistor_value))
+    c.drawCentredString(rect.left + rect.width*3/4, rect.bottom + rect.height/13, get_4digit_code(resistor_value))
+    c.drawRightString(rect.left + rect.width - rect.width/32, rect.bottom + rect.height/13, get_eia98_code(resistor_value))
 
-def main():
 
-    c = canvas.Canvas("ResistorLabels.pdf", pagesize=letter)
+def render_stickers(c, values):
 
-    draw_resistor_sticker(c, 0, 0, 9.99999)
-    draw_resistor_sticker(c, 0, 1, 0.00055)
-    draw_resistor_sticker(c, 0, 2, 0.00551)
-    draw_resistor_sticker(c, 1, 0, 0.0552)
-    draw_resistor_sticker(c, 1, 1, 0.553)
-    draw_resistor_sticker(c, 1, 2, 5.54)
-    draw_resistor_sticker(c, 2, 0, 55.9)
-    draw_resistor_sticker(c, 2, 1, 555)
-    draw_resistor_sticker(c, 2, 2, 5560)
-    draw_resistor_sticker(c, 3, 0, 55700)
-    draw_resistor_sticker(c, 3, 1, 558000)
-    draw_resistor_sticker(c, 3, 2, 5590000)
-    draw_resistor_sticker(c, 4, 0, 5500000)
+    for (rowId, row) in enumerate(values):
+        for (columnId, value) in enumerate(row):
+            draw_resistor_sticker(c, rowId, columnId, value)
 
+
+def render_outlines(c):
     for y in range(3):
         for x in range(10):
             rect = StickerRect(x, y)
             c.setStrokeColor(black, 0.1)
             c.setLineWidth(0)
             c.roundRect(rect.left, rect.bottom, rect.width, rect.height, rect.corner)
+
+
+def main():
+
+    c = canvas.Canvas("ResistorLabels.pdf", pagesize=letter)
+
+    render_stickers(c, [
+        [  .1,  .02, .003],
+        [  11,   12,   13],
+        [  21,   22,   33],
+        [  31,   32,   33],
+        [  41,   42,   43],
+        [  51,   52,   53],
+        [  61,   62,   63],
+        [  71,   72,   73],
+        [  81,   82,   83],
+        [  91,   92,   33],
+    ])
+
     c.showPage()
     c.save()
 
